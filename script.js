61683117a9f5e85d1b8123d35a130389cf0c38b3
Tamara Szuchet <tamara.szuchet@coyotes.usd.edu>
2026-06-20 19:05:01 +0000

    Followed prompts

--- script.js
+++ script.js
@@ -3,17 +3,22 @@ document.getElementById("genre").addEventListener("change", function() {
   // Get the selected genre
   const selectedGenre = document.getElementById("genre").value;
 
-  // Add movies to the movieList based on the selected genre
+  let movieList = [];
+
+  // Push movies to the movieList based on the selected genre
   if (selectedGenre === "Comedy") {
-    console.log("You selected 'Comedy'");
+    movieList.push("Superbad", "Bridesmaids", "The Hangover", "Step Brothers");
   } else if (selectedGenre === "Action") {
-    console.log("You selected 'Action'");
+    movieList.push("Die Hard", "Mad Max: Fury Road", "John Wick", "The Dark Knight");
   } else if (selectedGenre === "Drama") {
-    console.log("You selected 'Drama'");
+    movieList.push("The Shawshank Redemption", "Forrest Gump", "The Godfather", "Fences");
   } else if (selectedGenre === "Sci-Fi") {
-    console.log("You selected 'Sci-Fi'");
+    movieList.push("Inception", "Interstellar", "Blade Runner", "The Matrix");
   }
 
+  // Pick a random movie from the list
+  const randomMovie = movieList[Math.floor(Math.random() * movieList.length)];
+
   // Display the list of movies on the page
-  document.getElementById("movieRecommendations").innerText = `Enjoy: !`;
+  document.getElementById("movieRecommendations").innerText = `Enjoy: ${randomMovie}!`;
 });
